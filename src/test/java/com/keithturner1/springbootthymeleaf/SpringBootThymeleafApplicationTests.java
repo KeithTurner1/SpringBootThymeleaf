5dea783f80e291f1d3bf9880f5c2d6dcecbc248e
Keith <turnerkj_git@protonmail.com>
2021-05-30 09:25:19 -0400

    Initial Commit

---
+++ src/test/java/com/keithturner1/springbootthymeleaf/SpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keithturner1.springbootthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
